1d8f18989e403678a5701f3ca9f9ca50b2b82cf5
dennis mcmorris <dennis.mcmorris@icloud.com>
2023-10-22 19:56:41 -0500

    first commit

---
+++ circumference.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def circumference():
+    radius = int(input("What is the radius of the circle? "))
+    circum = radius * pi * 2
+    print(circum)
+    
+circumference()   
+
+
+
---
+++ square.py
@@ -0,0 +1,8 @@
+def square():
+    length = int(input("What is the length of your house? "))
+    width = int(input("What is the width of your house? "))
+    area = length * width
+    print(f'The area of your house is {area} sq ft')
+
+square()
+
